66a1fd77c52d9ac2411e4fdb7579f33d8ddd3794
FranMT-S <franciscomadridtoledo@gmail.com>
2022-10-31 22:37:39 -0600

    sidenav terminado, rutas colocadas en admin

--- MakeBusiness/Fronted/src/app/admin/admin-routing.module.ts
+++ MakeBusiness/Fronted/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent,
+    children:[
+      {
+        path:"companies",
+        loadChildren: () => import('./pages/companies/companies.module').then(module => module.CompaniesModule)
+      },
+      {
+        path:"plains",
+        loadChildren: () => import('./pages/plains/plains.module').then(module => module.PlainsModule)
+      },
+      {
+        path:"templates",
+        loadChildren: () => import('./pages/templates/templates.module').then(module => module.TemplatesModule)
+      },
+      {
+        path:"users",
+        loadChildren: () => import('./pages/users/users.module').then(module => module.UsersModule)
+      },
+    ]
+  }
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- MakeBusiness/Fronted/src/app/admin/admin.module.ts
+++ MakeBusiness/Fronted/src/app/admin/admin.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { CompaniesModule } from './pages/companies/companies.module';
+import { PlainsModule } from './pages/plains/plains.module';
+import { TemplatesModule } from './pages/templates/templates.module';
+import { UsersModule } from './pages/users/users.module';
+import { HomeComponent } from './home/home.component';
+import { MaterialModule } from '../material/material.module';
+
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule,
+    CompaniesModule,
+    PlainsModule,
+    TemplatesModule,
+    UsersModule,
+    MaterialModule,
+    
   ]
 })
 export class AdminModule { }
